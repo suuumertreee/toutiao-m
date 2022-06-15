6cdb51f35a9c6ac1622c16576986a9026361db20
goauleou <goauleou88@gmail.com>
2022-06-15 12:48:21 +0800

    项目初始化--调整初始化结构目录

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
+    <h2>黑马头条</h2>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: 'App', // 建议每个组件都给一个 name 属性值
+  data () {
+    return {
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
     }
   }
 }
+</script>
+<style lang="less">
+
 </style>
